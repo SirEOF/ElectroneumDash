3b019340b8b52efecbb42eaa97e7beef3fdbd2e2
Joeslie <jlrain98>
2017-12-09 00:06:11 +0800

    First Commit / Clean Program

---
+++ app/src/main/java/com/example/joeslie/myapplication/DashboardAct.java
@@ -0,0 +1,15 @@
+package com.example.joeslie.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashboardAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+    }
+}
